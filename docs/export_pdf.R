67229e83991486c056fc9643474b5cde05faf652
yaochung41 <yaochung41@gmail.com>
2022-08-14 17:35:19 +0200

    first html and pdf version of cv

---
+++ docs/export_pdf.R
@@ -0,0 +1,9 @@
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("index.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "index.pdf")
